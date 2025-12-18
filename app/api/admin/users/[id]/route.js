344b102629581906e0d34b8e9ee506ba410d975c
WebGomezPalacio <luisjaviermtzb@gmail.com>
2025-12-17 18:59:23 -0600

    feat: add more features

--- app/api/admin/users/[id]/route.js
+++ app/api/admin/users/[id]/route.js
@@ -1,13 +1,14 @@
 import { NextResponse } from 'next/server';
 import User from '@/models/User';
 import { getServerSession } from "next-auth";
-import { handler } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/authOptions"; 
 
 // DELETE: Eliminar usuario
 export async function DELETE(request, { params }) {
   try {
-    // Seguridad
-    const session = await getServerSession(handler);
+
+    const session = await getServerSession(authOptions); 
+    
     if (session?.user?.role !== 'admin') {
       return NextResponse.json({ message: 'No autorizado' }, { status: 401 });
     }
@@ -17,14 +18,17 @@ export async function DELETE(request, { params }) {
 
     return NextResponse.json({ message: 'Usuario eliminado correctamente' });
   } catch (error) {
+    console.error(error);
     return NextResponse.json({ message: 'Error al eliminar' }, { status: 500 });
   }
 }
 
-
+// PUT: Actualizar rol
 export async function PUT(request, { params }) {
   try {
-    const session = await getServerSession(handler);
+    // CAMBIO 3: Usar authOptions aquí también
+    const session = await getServerSession(authOptions);
+    
     if (session?.user?.role !== 'admin') return NextResponse.json({ message: 'No autorizado' }, { status: 401 });
 
     const { id } = params;
@@ -34,6 +38,7 @@ export async function PUT(request, { params }) {
 
     return NextResponse.json({ message: 'Rol actualizado' });
   } catch (error) {
+    console.error(error);
     return NextResponse.json({ message: 'Error al actualizar' }, { status: 500 });
   }
 }
