761a8f915f90edb5e6a53b519219cdb54f0b8ee8
WebGomezPalacio <luisjaviermtzb@gmail.com>
2025-12-17 19:09:33 -0600

    Initial commit

--- app/api/admin/users/[id]/route.js
+++ app/api/admin/users/[id]/route.js
@@ -2,12 +2,11 @@ import { NextResponse } from 'next/server';
 import User from '@/models/User';
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/authOptions"; 
-
 // DELETE: Eliminar usuario
 export async function DELETE(request, { params }) {
   try {
 
-    const session = await getServerSession(authOptions); 
+    const session = await getServerSession(authOptions);
     
     if (session?.user?.role !== 'admin') {
       return NextResponse.json({ message: 'No autorizado' }, { status: 401 });
@@ -26,7 +25,7 @@ export async function DELETE(request, { params }) {
 // PUT: Actualizar rol
 export async function PUT(request, { params }) {
   try {
-    // CAMBIO 3: Usar authOptions aquí también
+ 
     const session = await getServerSession(authOptions);
     
     if (session?.user?.role !== 'admin') return NextResponse.json({ message: 'No autorizado' }, { status: 401 });
